ff4dc5ea3987a05cde9706a56b7d25d45fa9fc76
dchung117 <dchung0330@gmail.com>
2023-06-09 01:03:30 +0000

    created plotting function for game durations.

--- models/utils.py
+++ models/utils.py
@@ -1,6 +1,9 @@
 import random
-from typing import Any
+from typing import Any, Optional
 from collections import namedtuple, deque
+
+import matplotlib.pyplot as plt
+import torch
 
 Transition = namedtuple("Transition", ("state", "action" "next_state", "reward"))
 
@@ -33,3 +36,35 @@ class ReplayBuffer(object):
         :rtype: list
         """
         return random.sample(self.memory, batch_size)
+
+def plot_durations(durations: list[int], title: Optional[str] = None, show_result: bool = False) -> None:
+    """
+    Plot the durations of each episode over the training.
+
+    :param durations - list of episode durations
+    :dtype: list[int]
+    :param title - optional title for figure = None
+    :dtype: str
+    :param: show_result - flag of whether to show figure = False
+    :dtype: bool
+    """
+    plt.figure(1)
+    t = torch.tensor(durations, dtype=torch.float32)
+    if show_result:
+        plt.title("Result")
+    else:
+        plt.clf()
+        plt.title("Training")
+    plt.xlabel("Episode")
+    plt.ylabel("Duration")
+    plt.plot(t.numpy())
+
+    if len(durations) >= 100: # moving average over 100 episodes
+        means = t.unfold(0, 1000, 1).mean(dim=1).view(-1)
+        means = torch.cat((torch.zeros(99), means), dim=0)
+        plt.plot(means.numpy())
+
+    if title:
+        plt.savefig(f"{title}.png")
+    else:
+        plt.savefig("durations.png")
